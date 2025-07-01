588e4d2c4a7f4147993c6a34cad4ca0f2254be89
baothanh4 <taobaothanh365@gmail.com>
2025-07-01 09:36:31 +0700

    them spawn enemy

---
+++ Assets/PROJECT/Script/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;             // Gán prefab zombie vào đây
+    public Transform[] spawnPoints;            // Các vị trí spawn
+    public float spawnInterval = 3f;           // Bao lâu spawn 1 lần
+    public int maxEnemies = 10;                // Giới hạn số lượng
+
+    private int enemiesSpawned = 0;
+
+    void Start()
+    {
+        StartCoroutine(SpawnLoop());
+    }
+
+    IEnumerator SpawnLoop()
+    {
+        while (enemiesSpawned < maxEnemies)
+        {
+            SpawnEnemy();
+            enemiesSpawned++;
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int index = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[index];
+
+        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
---
+++ Assets/PROJECT/Script/HealthBarController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Health targetHealth;         // Kéo vào trong Inspector
+    public Slider healthSlider;         // Kéo Slider vào đây
+
+    void start()
+    {
+        if (targetHealth == null)
+            targetHealth = GetComponentInParent<Health>();
+    }
+    void Update()
+    {
+        if (targetHealth != null && healthSlider != null)
+        {
+            healthSlider.value = targetHealth.GetHealthPercent();
+        }
+    }
+}
